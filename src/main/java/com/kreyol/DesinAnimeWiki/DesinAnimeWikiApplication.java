7f5ae1fe75ced7a5fe2d8289e1dfd3a005a515a7
Dwene Louis <Dwenegt@yahoo.com>
2024-12-27 00:26:12 -0500

    Initial

---
+++ src/main/java/com/kreyol/DesinAnimeWiki/DesinAnimeWikiApplication.java
@@ -0,0 +1,13 @@
+package com.kreyol.DesinAnimeWiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesinAnimeWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesinAnimeWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kreyol/DesinAnimeWiki/DesinAnimeWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreyol.DesinAnimeWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesinAnimeWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
